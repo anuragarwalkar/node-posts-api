d4fb9946e11b4fa6f8e61020e2140ba0460d621c
anurag <anuragarwalkar@gmail.com>
2019-10-05 21:35:05 +0530

    img Upload

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
+const multer = require('multer');
+
+const MIME_TYPE_MAP = {
+    'image/png':'png',
+    'image/jpeg':'jpg',
+    'image/jpg':'jpg'
+}
+const imgStorage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let err = new Error('invalid mime type')
+        if(isValid){
+            err = null
+        }
+        cb(err,'./images')
+    },
+    filename:(req,file,cb)=>{
+        const name = file.originalname.toLowerCase();
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null,`${name}-${Date.now()}.${ext}`)
+    }
+})
 
 router.get('/',async (req,res)=>{
     const allPosts = await Post.find();
     res.send(allPosts);
 });
 
-router.post('/',async(req,res)=>{
+router.post('/',multer({storage:imgStorage}).single('image'),async(req,res)=>{
     const newPost = new Post({
         title:req.body.title,
         description:req.body.description
