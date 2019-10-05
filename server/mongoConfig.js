ea8e7f85de6bc0d69c754c3ac095d8a014f3a6fa
anurag <anuragarwalkar@gmail.com>
2019-10-05 21:39:23 +0530

    img Upload

--- server/mongoConfig.js
+++ server/mongoConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/posts',{ useUnifiedTopology: true,useNewUrlParser: true,});
+mongoose.connect('mongodb+srv://anurag:tcN4hVD7cVJUHH9q@cluster0-mbclo.gcp.mongodb.net/posts?retryWrites=true&w=majority',{ useUnifiedTopology: true,useNewUrlParser: true,});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
