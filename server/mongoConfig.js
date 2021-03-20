8b2e01cf1a9a35f530de1e0939464a9dc07b3356
Anurag Arwalkar <anuragarwalkar@gmail.com>
2021-03-20 13:02:59 +0530

    Update mongoConfig.js

--- server/mongoConfig.js
+++ server/mongoConfig.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const password = process.env.MONGODB_PASSWORD
 
-mongoose.connect('mongodb+srv://anurag:tcN4hVD7cVJUHH9q@cluster0-mbclo.gcp.mongodb.net/posts?retryWrites=true&w=majority',{ useUnifiedTopology: true,useNewUrlParser: true,}).then(connection=>{
+mongoose.connect(`mongodb+srv://anurag:${password}@cluster0-mbclo.gcp.mongodb.net/posts?retryWrites=true&w=majority`,{ useUnifiedTopology: true,useNewUrlParser: true,}).then(connection=>{
     console.log('mongoDb:',connection.connection.host)
 }).catch(err=>{
     console.log('err:', err)
